98f7846429a8f76cec430194589aa1be34499f2b
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-08-06 15:09:12 +0900

    ヘッダー修正

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
     $(window).on('scroll', function () {
-        if (window.matchMedia('(min-width:768px)').matches) {
-            if ($('#top').height() < $(this).scrollTop()) {
-                $('.header').addClass('fixType');
-                $('.logo_img02').fadeIn(200);
-                $('.logo_img01').fadeOut(200);
-            } else {
-                $('.header').removeClass('fixType');
-                $('.logo_img01').fadeIn(200);
-                $('.logo_img02').fadeOut(200);
-            }
+        if ($('#top').height() < $(this).scrollTop()) {
+            $('.header').addClass('fixType');
+            $('.logo_img02').fadeIn(200);
+            $('.logo_img01').fadeOut(200);
+        } else {
+            $('.header').removeClass('fixType');
+            $('.logo_img01').fadeIn(200);
+            $('.logo_img02').fadeOut(200);
         }
-        if (window.matchMedia('(max-width:767px)').matches) {
-            if ($('#top').height() < $(this).scrollTop()) {
-                $('.logo_img03').fadeIn(200);
-                $('.logo_img01').fadeOut(200);
-            } else {
-                $('.logo_img01').fadeIn(200);
-                $('.logo_img03').fadeOut(200);
-            }
+    });
+
+    $(window).on('load resize', function () {
+        if (window.matchMedia('(min-width:768px)').matches) {
+            $('.logo_img02').attr("src", "assets/images/fixed_logo.svg");
+        } else {
+            $('.logo_img02').attr("src", "assets/images/menu_logo.svg");
         }
     });
 });
